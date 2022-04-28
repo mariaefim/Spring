01808f7583268ad65657fe86f435a780d89163f2
mariaefim <91070989+mariaefim@users.noreply.github.com>
2022-04-28 19:24:17 +0300

    Update ex2.py

--- Lab 0/ex2.py
+++ Lab 0/ex2.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(-10, 10.01, 0.01)
+x = np.arange(-10, 10, 0.01)
 plt.plot(x,  x*x - x - 6)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+plt.grid(True)
 plt.show()
